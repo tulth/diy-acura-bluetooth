09a93f99db92b74ff878964cd991dfb0e01859a1
tulthix <tulthix@gmail.com>
2016-03-05 20:49:36 -0600

    fix: MBUS_END_MSG_CODE is now in nibbles

--- target_desktop/py_lib/mbus/toggle_phy_rx_so.py
+++ target_desktop/py_lib/mbus/toggle_phy_rx_so.py
@@ -64,7 +64,7 @@ def toggleSim(lowLevelToggleSeq):
         if not libMbus.mbus_phy_rx_is_empty(ctypes.byref(mbusPhy)):
             while not libMbus.mbus_phy_rx_is_empty(ctypes.byref(mbusPhy)):
                 newNib = libMbus.mbus_phy_rx_pop(ctypes.byref(mbusPhy))
-                if newNib == MBUS_END_MSG_CODE:
+                if newNib == nibbles.MBUS_END_MSG_CODE:
                     # print("{}".format("".join(rxNibList)))
                     # print("{}".format(rxNibList))
                     rxNibListList.append(rxNibList)
